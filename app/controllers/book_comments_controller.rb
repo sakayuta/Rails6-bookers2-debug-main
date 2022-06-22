85a249d71d72830966c7967f48bd3a01eefd2642
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-06-22 10:06:46 +0000

    [Finish 応用課題3] Rails6-bookers2-debug-main

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,8 +6,14 @@ class BookCommentsController < ApplicationController
     comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book.id)
+    redirect_to book_path(params[:book_id])
   end
+  
+  def destroy
+    BookComment.find(params[:id]).destroy
+    redirect_to book_path(params[:book_id])
+  end
+  
   
   
   private
