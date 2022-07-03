b0c75d5de6f2ddbc5a06a0534070dfa4fc567df4
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-07-03 06:48:35 +0000

    [Start add_ association 応用課題4] Rails6-bookers2-debug-main

---
+++ test/controllers/relationships_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class RelationshipsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get relationships_index_url
+    assert_response :success
+  end
+end
