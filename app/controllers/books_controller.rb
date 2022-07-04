5ae9e1f536a6360d706228d174e7961aa94ac398
EC2 Default User <ec2-user@ip-172-31-11-35.ap-northeast-1.compute.internal>
2022-07-04 10:39:26 +0000

    [Finish 応用課題5] Rails6-bookers2-debug-main

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,9 +27,7 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-    if @book.user == current_user
-       render :edit
-    else
+    if @book.user != current_user
        redirect_to books_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "homes#top"
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers'  => 'relationships#followers', as: 'followers'
   end
-  
+
+  get "search" => "searches#search"
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
